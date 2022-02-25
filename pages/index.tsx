e58e88ce241ea19a60648754206c6b2433ffd034
Radovan Šurlák <radovan.surlak@gmail.com>
2022-02-25 15:18:08 +0700

    added donation button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import type { NextPage } from "next";
 import { Dove } from "../components/Dove";
 import { UkraineFlag } from "../components/UkraineFlag";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const [showHelpUkraine, setshowHelpUkraine] = useState(false);
+
+  function helpUkraine() {
+    window.location.href = "https://savelife.in.ua/en/donate/";
+  }
+
   return (
-    <div className={styles.container}>
-      <Dove />
-      <UkraineFlag />
+    <div onClick={helpUkraine} className={styles.container}>
+      <div className={styles.wrapper}>
+        <Dove />
+        <UkraineFlag />
+        <button className={styles.helpUkraineButton} onClick={helpUkraine}>
+          🕊 Support ❤️ Ukraine 🇺🇦
+        </button>
+      </div>
     </div>
   );
 };
