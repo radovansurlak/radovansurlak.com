d89d81842950b3e85e80f2325d9e6f09c7c93316
Radovan Šurlák <radovan.surlak@gmail.com>
2022-02-26 14:03:55 +0700

    added support title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,19 @@
-import { useState } from "react";
 import type { NextPage } from "next";
+import Head from "next/head";
 import { Dove } from "../components/Dove";
 import { UkraineFlag } from "../components/UkraineFlag";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  const [showHelpUkraine, setshowHelpUkraine] = useState(false);
-
   function helpUkraine() {
     window.location.href = "https://savelife.in.ua/en/donate/";
   }
 
   return (
     <div onClick={helpUkraine} className={styles.container}>
+      <Head>
+        <title>🕊 Support ❤️ Ukraine 🇺🇦</title>
+      </Head>
       <div className={styles.wrapper}>
         <Dove />
         <UkraineFlag />
